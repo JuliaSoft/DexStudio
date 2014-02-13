5121f333813e0ecf13f3394d062bc9614c57f194
eugenioancona <id726gbw@studenti.univr.it>
2014-02-13 16:55:20 +0100

    Log console formatting added

--- main/src/com/juliasoft/dexstudio/log/DexLog.java
+++ main/src/com/juliasoft/dexstudio/log/DexLog.java
@@ -1,6 +1,8 @@
 package com.juliasoft.dexstudio.log;
 
 import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.swing.JComboBox;
@@ -9,7 +11,10 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Appender;
@@ -30,6 +35,18 @@ public class DexLog extends JPanel
 {
 	private JTextPane log = new JTextPane();
 	private JComboBox<String> selection = new JComboBox<String>();
+	private final String fontSize = 14 + "pt";
+	private final String fontName = getFont().getName();
+	private final String bodyStyle = "body{font-family:" + fontName + "font-size:" + fontSize + ";font-weight:normal;}";
+	private final String warnStyle = ".warn{background-color:#EEEE66;}";
+	private final String errorStyle = ".error{color:red;}";
+	private final String debugStyle = ".debug{background-color:#DDDDFF}";
+	private final String divStyle = "div{border-bottom: 1px solid black; padding: 3px 2px};";
+
+	
+	private final String htmlStart = "<html><head><style type='text/css'>"+ bodyStyle + warnStyle + errorStyle + debugStyle + divStyle +"</style></head><body>";
+	private final String htmlClose = "</body></html>";
+	//private HTMLDocument doc;
 	
 	private final class SwingAppender extends AbstractAppender implements Appender
 	{
@@ -46,15 +63,32 @@ public class DexLog extends JPanel
 		@Override
 		public void append(LogEvent event)
 		{
-			Document doc = DexLog.this.log.getDocument();
-			try
-			{
-				doc.insertString(doc.getLength(), getLayout().toSerializable(event).toString(), null);
-			}
-			catch(BadLocationException e)
-			{
+			
+			HTMLDocument doc = (HTMLDocument)log.getDocument();
+			Element body = doc.getElement(doc.getDefaultRootElement(), StyleConstants.NameAttribute, HTML.Tag.BODY);
+			
+			String str = getLayout().toSerializable(event).toString();
+			
+			if(str.contains("WARN"))
+				
+				str = "<div class='warn'>" + str + "</div>";
+			
+			else if(str.contains("ERROR"))
+				
+				str = "<div class='warn'>" + str + "</div>";
+			
+			else if(str.contains("DEBUG"))
+				
+				str = "<div class='debug'>" + str + "</div>";
+			
+			try {
+				doc.insertBeforeEnd(body, str);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			log.setCaretPosition(doc.getLength());
 		}
 	}
 	
@@ -65,9 +99,15 @@ public class DexLog extends JPanel
 	{
 		super(new BorderLayout());
 		log.setEditable(false);
+		log.setContentType("text/html");
+		log.setText(htmlStart + htmlClose);
+		log.setMargin(new Insets(0,0,0,0));
+		/*DefaultCaret c = (DefaultCaret)log.getCaret();
+		c.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);*/
+		
 		JPanel header = new JPanel(new BorderLayout());
 		JScrollPane scroll = new JScrollPane(log);
-		selection.addItem("Option 1");
+		selection.addItem("Show All");
 		selection.addItem("Option 2");
 		selection.addItem("Option 3");
 		header.add(new JLabel("Log"), BorderLayout.WEST);
@@ -88,16 +128,4 @@ public class DexLog extends JPanel
 		l.addAppender(new SwingAppender());
 	}
 	
-	/**
-	 * Append log string to the console
-	 * 
-	 * @param str
-	 */
-	public void log(String str)
-	{
-		if(log.getText() == "")
-			log.setText(str);
-		else
-			log.setText(log.getText() + "\n" + str);
-	}
 }
